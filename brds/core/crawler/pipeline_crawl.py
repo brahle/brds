590625babef7a1b468091926e0420233c666b55f
Bruno Rahle <brahle@gmail.com>
2024-08-18 23:27:15 +0100

    Fixing Crawler

--- brds/core/crawler/pipeline_crawl.py
+++ brds/core/crawler/pipeline_crawl.py
@@ -14,13 +14,13 @@ class PipelineCrawler(Crawler):
         super(PipelineCrawler, self).__init__(*args, **kwargs)
         self.links_from_table = [LinksFromTable(**remove_default_params(url)) for url in self.urls]
 
-    def process(self: "PipelineCrawler", variables: VariableHolder) -> None:
+    async def process(self: "PipelineCrawler", variables: VariableHolder) -> None:
         for links in self.links_from_table:
             for url in links.load(variables):
                 url_id = self.database.register_web_page(url)
                 self.database.set_vriables(url_id, variables.variables)
                 if self.should_load(url_id, True):
-                    self.download(url, url_id)
+                    await self.download(url, url_id)
                 else:
                     LOGGER.info(f"Will not download '{url}', as I've already downloaded it")
 
--- brds/core/crawler/root_crawler.py
+++ brds/core/crawler/root_crawler.py
@@ -14,12 +14,12 @@ class RootCrawler(Crawler):
         super(RootCrawler, self).__init__(*args, **kwargs)
         self.templated_urls = [TemplatedUrl(database=self.database, **remove_default_params(url)) for url in self.urls]
 
-    def process(self: "RootCrawler", variables: VariableHolder) -> None:
+    async def process(self: "RootCrawler", variables: VariableHolder) -> None:
         for templated_url in self.templated_urls:
             url = templated_url.resolve(variables)
             url_id = self.database.register_web_page(url)
             self.database.set_vriables(url_id, variables.variables)
             if self.should_load(url_id, templated_url.cache):
-                self.download(url, url_id)
+                await self.download(url, url_id)
             else:
                 LOGGER.info(f"Will not download '{url}', as I've already downloaded it")
